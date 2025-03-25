2d93f2d05cc04c0a842c91e08287593fd25c0a70
ProgrammerRaqib <raqibjokhio77@gmail.com>
2025-03-25 10:31:04 +0500

    bkasn

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-  
-<div className="Maindiv">
-
-    <div className='innerDiv'>
-      
-      <div>
-        
-        <h2>CountDown & Light</h2> <button><div> <div className='Butn'></div></div></button>
-        <h2>Switch</h2>
-       
-        <div className='Line'></div>
-       
-
-
-
-      </div>
-
-    </div>
-
-
-
-</div>
+div>
 
 
 
